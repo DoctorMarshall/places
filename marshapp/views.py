fa6a0d5c22d1b8d2ea0e4bba19db459153558f12
DoctorMarshall <and.marszalek@gmail.com>
2014-06-15 20:02:34 +0200

    Update views.py

--- marshapp/views.py
+++ marshapp/views.py
@@ -131,9 +131,9 @@ class LoginView(View):
                     </style>
                 <body>
                     <h1 style="text-align:center;">Pizza Finder</h1>
-                    <form style="text-align:left;" method="GET" action="">{% csrf_token %}
+                    <form method="GET" action="">{% csrf_token %}
                         Login: <input align="center" type="text" name="login"><br>
-                        Password: <input  align="center"type="password" name="pass"><br><br>
+                        Password: <input align="center" type="password" name="pass"><br><br>
 
                         <input align="right" type="submit" value="Login/Register">
                     </form> 
